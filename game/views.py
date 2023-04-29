ebf7d7a55f765c569a0f31ddac858368b2ce0da0
dlwsdqdws <dlwsdqdws@gmail.com>
2023-04-30 04:15:26 +0800

    game test page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align:center">Battle Balls</h1>'
+    line4 = '<a href="/play/">Go to Play</a>'
+    line2 = '<img src="https://thumb.idongdong.com/2019-09/18/16f5d81f24a6ed06.jpeg" width=2000 />'
+    line3 = '<hr>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+def play(request):
+    line1 = '<h1>Play Page</h1>'
+    line2 = '<a href="/">Back</a>'
+    return HttpResponse(line1 + line2)
