0c099d030729fab9085de0aa3e8d54ed3c3e2780
dlwsdqdws <dlwsdqdws@gmail.com>
2023-05-01 02:52:43 +0800

    add player class

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -130,6 +130,41 @@ class GameMap extends BallGameObject {
         this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
 }
+class Player extends BallGameObject{
+    constructor(playground, x, y, radius, color, speed, is_me){
+        super();
+
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.speed = speed;
+        this.radius = radius;
+        this.is_me = is_me;
+        // float computing
+        this.eps = 0.1;
+    }
+
+    start(){
+
+    }
+
+    update(){
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+
+    on_destroy(){
+
+    }
+}
 class BallGamePlayground {
     constructor(root){
         this.root = root;
@@ -142,6 +177,8 @@ class BallGamePlayground {
         this.width = this.$playground.width();
         this.height = this.$playground.height();
         this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true));
 
         this.start();
     }
