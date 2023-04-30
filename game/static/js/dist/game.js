771bc229786deb0b3799eb0d92a04a9b6cff6554
dlwsdqdws <dlwsdqdws@gmail.com>
2023-05-01 06:43:33 +0800

    add particle when hit

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -130,6 +130,51 @@ class GameMap extends BallGameObject {
         this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
 }
+class Particle extends BallGameObject {
+    constructor(playground, x, y, radius, vx, vy, color, speed, move_len){
+        super();
+
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.vx = vx;
+        this.vy = vy;
+        this.color = color;
+        this.speed = speed;
+        this.move_len = move_len;
+        this.friction = 0.9;
+        this.eps = 1;
+    }
+
+    start(){
+
+    }
+
+    update(){
+        if (this.move_len < this.eps || this.speed < this.eps){
+            this.destroy();
+            return false;
+        }
+        else{
+            let moved = Math.min(this.move_len, this.speed * this.timedelta / 1000);
+            this.x += this.vx * moved;
+            this.y += this.vy * moved;
+            this.speed *= this.friction;
+            this.move_len -= moved;
+        }
+
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+}
 class Player extends BallGameObject{
     constructor(playground, x, y, radius, color, speed, is_me){
         super();
@@ -238,6 +283,20 @@ class Player extends BallGameObject{
 
         // Movement speed is halved when hit
         this.speed *= 1.25;
+
+        // particle effect
+        for (let i = 0; i < 10 + Math.random() * 5; i ++ ) {
+            let x = this.x;
+            let y = this.y;
+            let radius = this.radius * Math.random() * 0.1;
+            let angle = 2 * Math.PI * Math.random();
+            let vx = Math.cos(angle);
+            let vy = Math.sin(angle);
+            let color = this.color;
+            let speed = this.speed * 10;
+            let move_len = this.radius * Math.random() * 10;
+            new Particle(this.playground, x, y, radius, vx, vy, color, speed, move_len);
+        }
     }
 
     update(){
--- readme.md
+++ readme.md
@@ -8,4 +8,8 @@
 ## Operations
 1. Right click mouse to move;
 2. Press keyboard 'Q' to choose skill - FireBall;
-3. Left Click mouse to attack (always choose a skill before attaching).
+3. Left Click mouse to attack (always choose a skill before attaching). 
+
+## Effects
+1. After being attacked, the radius of the ball is halved and the speed is increased by 25%. One ball is out when its radius is less than zero.
+2. When hit, the ball will randomly release 10-15 small particles. The color of the particle is the same as that of the ball, the speed is ten times that of the ball, and other parameters are randomly generated.
